d3bb8a012b4a917a6bd8b2c941cc36e1b64c0924
Rémi Lecas <lecasremi@gmail.com>
2023-05-04 11:12:00 +0200

    Ajout de commentaire + ajouts de la liste des voitures présentes dans le contrat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import Web3 from "web3";
-import Wallet from './contracts/Wallet.json';
 import { useEffect, useState } from "react";
 
 import CarsContract from "./contracts/CarsContract.json";
@@ -7,7 +6,6 @@ import CarsContract from "./contracts/CarsContract.json";
 function App() {
   const [contract, setContract] = useState(null);
   const [accounts, setAccounts] = useState([]);
-  const [contrat, setContrat] = useState(null);
   const [web3, setWeb3] = useState(null);
   const [marque, setMarque] = useState("");
   const [modele, setModele] = useState("");
@@ -20,46 +18,40 @@ function App() {
 
   const [balance, setBalance] = useState();
 
-  const [currentAccount, setCurrentAccount] = useState();
 
-  //Init mon application
   async function init() {
-    // Je lance mon check de compte
-    await checkAccount();
-  }
-
-
-  async function checkAccount() {
     if (window.ethereum) {
 
       try {
+
+        // demande la connexion à metamask
         await window.ethereum.enable();
 
-        // J'init maintenant mon Web3 pour obtenir mon contrat déployé
+        // J'init de mon web3
         const web3 = new Web3(window.ethereum);
-        console.log(web3);
+        setWeb3(web3);
 
         // Récupération du compte MetaMask
         const accounts = await web3.eth.getAccounts();
-        console.log(accounts);
-
-        setWeb3(web3);
         setAccounts(accounts);
 
+        // Initialisation du contrat
+        // Récupération du networkId
         const networkId = await web3.eth.net.getId();
-        console.log({ networkId: networkId })
+        // Récupération du network déployé grâce au network Id
         const deployedNetwork = CarsContract.networks[networkId];
-        console.log({ deployedNetwork: deployedNetwork })
 
         const contract = new web3.eth.Contract(
           CarsContract.abi,
           deployedNetwork && deployedNetwork.address,
         );
 
-        console.log({ contract: contract })
-
-
         setContract(contract);
+
+        // récupération de toutes mes voitures
+        const allCars = await contract.methods.getAllCars().call();
+        setCars(allCars);
+
       } catch (error) {
         console.error(error);
       }
@@ -67,14 +59,6 @@ function App() {
       console.error("Metamask n'est pas installé sur cette application")
     }
   }
-
-  // const initWeb3 = async () => {
-  //   const networkId = await web3.eth.net.getId();
-  //   const deployedNetwork = CarsContract.networks[networkId];
-  //   const instance = new web3.eth.Contract(CarsContract.abi, deployedNetwork && deployedNetwork.address);
-  //   setWeb3(web3);
-  //   setContrat(instance);
-  // };
 
   async function getBalanced() {
     // const count = contract.methods.getBalance();
@@ -87,17 +71,15 @@ function App() {
     event.preventDefault();
     await contract.methods.addCars(marque, modele, fuel, color, power, annee).send({ from: accounts[0] });
 
+    // J'appelle toute mes voitures pour actualiser ma liste
+    getAllCars();
+
   }
 
-  const getAllCars = async (event) => {
-    event.preventDefault();
-
+  async function getAllCars() {
     const allCars = await contract.methods.getAllCars().call();
-
     console.log(allCars);
     setCars(allCars);
-
-
   }
 
   useEffect(() => {
@@ -113,65 +95,77 @@ function App() {
         <p>{balance}</p>
 
         <hr />
+        <h2>Form add cars</h2>
 
         <form onSubmit={addCars}>
-          <label>Enter your marque:
-            <input
-              type="text"
-              value={marque}
-              onChange={(e) => setMarque(e.target.value)}
-            />
-          </label>
-          <label>Enter your modele:
-            <input
-              type="text"
-              value={modele}
-              onChange={(e) => setModele(e.target.value)}
-            />
-          </label>
-          <label>Enter your fuel:
-            <input
-              type="text"
-              value={fuel}
-              onChange={(e) => setFuel(e.target.value)}
-            />
-          </label>
-          <label>Enter your color:
-            <input
-              type="text"
-              value={color}
-              onChange={(e) => setColor(e.target.value)}
-            />
-          </label>
-          <label>Enter your power:
-            <input
-              type="number"
-              value={power}
-              onChange={(e) => setPower(e.target.value)}
-            />
-          </label>
-          <label>Enter your annee:
-            <input
-              type="number"
-              value={annee}
-              onChange={(e) => setAnnee(e.target.value)}
-            />
-          </label>
+          <div>
+            <label>Enter your marque:
+              <input
+                type="text"
+                value={marque}
+                onChange={(e) => setMarque(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>Enter your modele:
+              <input
+                type="text"
+                value={modele}
+                onChange={(e) => setModele(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>Enter your fuel:
+              <input
+                type="text"
+                value={fuel}
+                onChange={(e) => setFuel(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>Enter your color:
+              <input
+                type="text"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>Enter your power:
+              <input
+                type="number"
+                value={power}
+                onChange={(e) => setPower(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>Enter your annee:
+              <input
+                type="number"
+                value={annee}
+                onChange={(e) => setAnnee(e.target.value)}
+              />
+            </label>
+          </div>
           <input type="submit" />
         </form>
-
+        
         <div>
-          <button onClick={getAllCars}>Get All Cars</button>
-        </div>
-
-        <div>
-          <ul>
-            {cars.map((car, index) => (
-              <li key={index}>
-                Marque : {car.marque} | Modèle : {car.modele} | Année : {car.annee} | Fuel : {car._fuel} | Color : {car._color} | Power : {car._power}
-              </li>
-            ))}
-          </ul>
+          <h2>List of cars</h2>
+          <div>
+            <ul>
+              {cars.map((car, index) => (
+                <li key={index}>
+                  Marque : {car.marque} | Modèle : {car.modele} | Année : {car.annee} | Fuel : {car._fuel} | Color : {car._color} | Power : {car._power}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
 
       </div>
